3a2df0e41942d4cdfee9c6f4d1b8c9443e4988de
AdiBot-max <adichauhan12354@gmail.com>
2025-11-24 15:46:46 +0530

    Update signup.js

--- public/signup.js
+++ public/signup.js
@@ -1,24 +1,39 @@
-
-const form = document.getElementById('loginForm');
+const form = document.getElementById('signupForm');
 
 form.addEventListener('submit', async (e) => {
   e.preventDefault();
 
-  const cubemail = document.getElementById('cubemail').value;
+  let cubemail = document.getElementById('cubemail').value.trim();
   const password = document.getElementById('password').value;
 
-  const res = await fetch('/login', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ cubemail, password })
-  });
+  if (!cubemail.endsWith("@cubemail.com")) {
+    return showAlert("Email must end with @cubemail.com", "error");
+  }
 
-  const data = await res.json();
+  if (!cubemail || !password) {
+    return showAlert("Both fields are required.", "error");
+  }
 
-  if (data.user) {
-    alert('Login successful!');
-    window.location.href = 'dashboard.html';
-  } else {
-    alert(data.error);
+  try {
+    const res = await fetch('/signup', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ cubemail, password })
+    });
+
+    const data = await res.json();
+
+    if (res.status >= 400) {
+      return showAlert(data.error?.message || "Signup error", "error");
+    }
+
+    showAlert("Account created!", "success");
+    setTimeout(() => {
+      window.location.href = "login.html";
+    }, 800);
+
+  } catch (err) {
+    console.error(err);
+    showAlert("Server error, try again later", "error");
   }
 });
